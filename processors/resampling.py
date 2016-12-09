916a3faac081c035fb9fa837c46d28999690b7f6
Jani Komppula <jani.komppula@cern.ch>
2016-12-09 12:04:17 +0100

    ADC,DAC, UpSampler and DownSampler tested

--- processors/resampling.py
+++ processors/resampling.py
@@ -17,13 +17,19 @@ from scipy import interpolate
 
 """
 
-# TODO: smoother DAC by using interpolation
-# TODO:
-
-
 class Resampler(object):
     def __init__(self,sampling_type, sampling_rate = None, signal_length = None, sync_method = 'bin_mid',
                  length_rounding = 'round', data_conversion = 'bin_average', store_signal  = False):
+        """
+
+        :param sampling_type:
+        :param sampling_rate:
+        :param signal_length:
+        :param sync_method:
+        :param length_rounding:
+        :param data_conversion:
+        :param store_signal:
+        """
 
         self._sampling_type = sampling_type
         self._sampling_rate = sampling_rate
@@ -36,6 +42,7 @@ class Resampler(object):
 
         self._length_rounding = length_rounding
         self._data_conversion = data_conversion
+
 
         self._n_bunches = None
 
@@ -70,7 +77,6 @@ class Resampler(object):
         self.required_variables = ['z_bins']
         self.label = 'Resampler'
 
-
     def process(self,bin_edges,signal,slice_sets,phase_advance, ** kwargs):
         if self._conversion_type is None:
             self.__init_variables(bin_edges,slice_sets)
@@ -87,13 +93,8 @@ class Resampler(object):
                 np.copyto(self._output_signal[output_from:output_to],
                           np.array(cython_matrix_product(self._conversion_matrix, np.array(signal[input_from:input_to]))))
         elif self._conversion_type == 'interpolation':
-            # f = interp1d(self._total_input_bin_mids, signal, kind='slinear')
-            #
-            # self._output_signal =  f(self._total_output_bin_mids)
-
             tck = interpolate.splrep(self._total_input_bin_mids, signal, s=0)
             self._output_signal = interpolate.splev(self._total_output_bin_mids, tck, der=0)
-            # self._output_signal = np.interp(self._total_output_bin_mids, self._total_input_bin_mids, signal)
         else:
             raise ValueError('Unknown value in Resampler._conversion_type')
 
@@ -117,12 +118,26 @@ class Resampler(object):
         self._total_input_bin_edges = np.copy(bin_edges)
         self._total_input_bin_mids = (self._total_input_bin_edges[:,0]+self._total_input_bin_edges[:,1])/2.
 
+        if isinstance(self._sampling_rate, float):
+            pass
+        elif isinstance(self._sampling_rate, int):
+            self._sampling_rate = float(self._sampling_rate)
+        elif self._sampling_rate is None:
+            self._sampling_rate = c/self._input_bin_spacing
+        elif isinstance(self._sampling_rate, tuple):
+            if self._sampling_rate[0] == 'multiplied':
+                self._sampling_rate = self._sampling_rate[1] * c/self._input_bin_spacing
+        else:
+            raise ValueError('Unknown value type in Resampler._sampling_rate')
+
+
+        if self._signal_length is None:
+            self._signal_length = self._input_z_bins[-1] - self._input_z_bins[0]
+
+
         if self._sampling_type == 'reconstructed':
             z_bins, n_slices_per_bunch, bin_spacing, sampling_rate, signal_length = \
                 self.__reconstruct_z_bins(self._signal_length, self._sampling_rate, self._input_z_bins)
-        elif self._sampling_type == 'resampled':
-            z_bins, n_slices_per_bunch, bin_spacing, sampling_rate, signal_length = \
-                self.__resample_z_bins(self._signal_length, self._sampling_rate, self._input_z_bins)
         elif self._sampling_type == 'original':
             z_bins = np.copy(slice_sets[0].z_bins) - np.mean(slice_sets[0].z_bins)
             n_slices_per_bunch = len(z_bins) -1
@@ -169,10 +184,12 @@ class Resampler(object):
                                            float(self._input_n_slices_per_bunch))
             self.__contruct_value_conversion_matrix(norm_coeff)
             self._conversion_type = 'matrix'
-        elif self._data_conversion == 'zero_bins':
-            # output bin is zero if the midpoint of the input bin is not in the bin
-            self.__contruct_value_conversion_matrix(None)
-            self._conversion_type = 'matrix'
+        elif isinstance(self._data_conversion,tuple):
+            if self._data_conversion[0] == 'kernel':
+                self.__contruct_value_conversion_matrix(self._data_conversion[1])
+                self._conversion_type = 'matrix'
+            else:
+                raise ValueError('Unknown value for Resampler._data_normalization')
         else:
             raise ValueError('Unknown value for Resampler._data_normalization')
 
@@ -261,19 +278,24 @@ class Resampler(object):
 
         return z_bins, n_slices_per_bunch, bin_spacing, sampling_rate, signal_length
 
-    def __contruct_value_conversion_matrix(self,norm_coeff):
+    def __contruct_value_conversion_matrix(self,conversion_value):
         self._conversion_matrix = np.zeros((len(self._output_z_bins) - 1, len(self._input_z_bins) - 1))
 
         for i, (i_min, i_max) in enumerate(zip(self._output_z_bins, self._output_z_bins[1:])):
             for j, (j_min, j_max) in enumerate(zip(self._input_z_bins, self._input_z_bins[1:])):
-                if norm_coeff is not None:
-                    self._conversion_matrix[i, j] = (self._CDF(i_max, j_min, j_max) -
-                                                     self._CDF(i_min, j_min, j_max)) * norm_coeff
+                if isinstance(conversion_value, float):
+                    self._conversion_matrix[i, j] = (self.__CDF(i_max, j_min, j_max) -
+                                                     self.__CDF(i_min, j_min, j_max)) * conversion_value
+                elif isinstance(conversion_value, list):
+                    if (i >= j * len(conversion_value)) and (i < (j+1) * len(conversion_value)):
+                        kernel_idx = i % len(conversion_value)
+                        self._conversion_matrix[i, j] = conversion_value[kernel_idx]
                 else:
-                    if (i_min <= (j_min + j_max)/2.) and ((j_min + j_max)/2. < i_max):
-                        self._conversion_matrix[i, j] = 1.
+                    raise ValueError('Unknown data type in conversion_value')
+        print 'self._conversion_matrix:'
+        print self._conversion_matrix
 
-    def _CDF(self,x,ref_bin_from, ref_bin_to):
+    def __CDF(self,x,ref_bin_from, ref_bin_to):
             if x <= ref_bin_from:
                 return 0.
             elif x < ref_bin_to:
@@ -282,7 +304,7 @@ class Resampler(object):
                 return 1.
 
 class Quantizer(object):
-    def __init__(self,n_bits,input_range, store_signal = True):
+    def __init__(self,n_bits,input_range, store_signal = False):
 
         """ Quantizates signal to discrete levels determined by the number of bits and input range.
         :param n_bits: the signal is quantized (rounded) to 2^n_bits levels
@@ -315,83 +337,157 @@ class Quantizer(object):
             self.output_signal = np.copy(output_signal)
             self.output_bin_edges = np.copy(bin_edges)
 
-        return output_signal
+        return bin_edges, output_signal
 
 
-# class ADC(object):
-#     def __init__(self,sampling_rate, n_bits = None, input_range = None, sync_method = 'round'):
-#         """ A model for an analog to digital converter, which changes a length of the input signal to correspond to
-#             the number of slices in the PyHEADTAIL. If parameters for the quantizer are given, it quantizes also
-#             the input signal to discrete levels.
-#         :param sampling_rate: sampling rate of the ADC [Hz]
-#         :param n_bits: the number of bits where to input signal is quantized. If the value is None, the input signal
-#                 is not quantizated. The default value is None.
-#         :param input_range: the range for for the quantizer. If the value is None, the input signal is not quantizated.
-#                 The default value is None.
-#         :param sync_method: The time range of the input signal might not correspond to an integer number of
-#             samples determined by sampling rate.
-#                 'rounded': The time range of the input signal is divided to number of samples, which correspons to
-#                     the closest integer of samples determined by the sampling rate (defaul)
-#                 'rising_edge': the exact value of the sampling rate is used, but there are empty space in the end
-#                     of the signal
-#                 'falling_edge': the exact value of the sampling rate is used, but there are empty space in the beginning
-#                     of the signal
-#                 'middle': the exact value of the sampling rate is used, but there are an equal amount of empty space
-#                     in the beginning and end of the signal
-#         """
-#         self._resampler = Resampler('reconstructed' , sampling_rate, sync_method)
-#         self.required_variables = copy.copy(self._resampler.required_variables)
-#
-#         self._digitizer = None
-#         if (n_bits is not None) and (input_range is not None):
-#             self._digitizer = Quantizer(n_bits,input_range)
-#             self.required_variables += self._digitizer.required_variables
-#
-#     def process(self,signal,slice_set, *args):
-#         signal = self._resampler.process(signal,slice_set)
-#
-#         if self._digitizer is not None:
-#             signal = self._digitizer.process(signal)
-#
-#         return signal
+class ADC(object):
+    def __init__(self,sampling_rate, n_bits = None, input_range = None, store_signal = False, **kwargs):
+        """ A model for an analog to digital converter, which changes a length of the input signal to correspond to
+            the number of slices in the PyHEADTAIL. If parameters for the quantizer are given, it quantizes also
+            the input signal to discrete levels.
+        :param sampling_rate: sampling rate of the ADC [Hz]
+        :param n_bits: the number of bits where to input signal is quantized. If the value is None, the input signal
+                is not quantizated. The default value is None.
+        :param input_range: the range for for the quantizer. If the value is None, the input signal is not quantizated.
+                The default value is None.
+        :param sync_method: The time range of the input signal might not correspond to an integer number of
+            samples determined by sampling rate.
+                'rounded': The time range of the input signal is divided to number of samples, which correspons to
+                    the closest integer of samples determined by the sampling rate (defaul)
+                'rising_edge': the exact value of the sampling rate is used, but there are empty space in the end
+                    of the signal
+                'falling_edge': the exact value of the sampling rate is used, but there are empty space in the beginning
+                    of the signal
+                'middle': the exact value of the sampling rate is used, but there are an equal amount of empty space
+                    in the beginning and end of the signal
+        """
+        self.label = 'ADC'
+        self._resampler = Resampler('reconstructed' , sampling_rate, **kwargs)
+        self.required_variables = copy.copy(self._resampler.required_variables)
 
-#
-# class DAC(object):
-#     def __init__(self,sampling_rate, n_bits = None, output_range = None, sync_method = 'round'):
-#         """ A model for an digital to analog converter, which changes a length of the input signal to correspond to
-#             the number of slices in the PyHEADTAIL. If parameters for the quantizer are given, it quantizes also
-#             the input signal to discrete levels.
-#         :param sampling_rate: sampling rate of the ADC [Hz]
-#         :param n_bits: the number of bits where to input signal is quantized. If the value is None, the input signal
-#                 is not quantizated. The default value is None.
-#         :param input_range: the range for for the quantizer. If the value is None, the input signal is not quantizated.
-#                 The default value is None.
-#         :param sync_method: The time range of the input signal might not correspond to an integer number of
-#             samples determined by sampling rate.
-#                 'rounded': The time range of the input signal is divided to number of samples, which correspons to
-#                     the closest integer of samples determined by the sampling rate (defaul)
-#                 'rising_edge': the exact value of the sampling rate is used, but there are empty space in the end
-#                     of the signal
-#                 'falling_edge': the exact value of the sampling rate is used, but there are empty space in the beginning
-#                     of the signal
-#                 'middle': the exact value of the sampling rate is used, but there are an equal amount of empty space
-#                     in the beginning and end of the signal
-#         """
-#         self._resampler = Resampler('DAC', sampling_rate, sync_method)
-#         self.required_variables = copy.copy(self._resampler.required_variables)
-#
-#         self._digitizer = None
-#         if (n_bits is not None) and (output_range is not None):
-#             self._digitizer = Quantizer(n_bits,output_range)
-#             self.required_variables += self._digitizer.required_variables
-#
-#     def process(self,signal,slice_set, *args):
-#
-#         if self._digitizer is not None:
-#             signal = self._digitizer.process(signal)
-#
-#         signal = self._resampler.process(signal,slice_set)
-#
-#         return signal
-#
-#
+        self._digitizer = None
+        if (n_bits is not None) and (input_range is not None):
+            self._digitizer = Quantizer(n_bits,input_range, *kwargs)
+            self.required_variables += self._digitizer.required_variables
+        elif (n_bits is not None) or (input_range is not None):
+            raise ValueError('Either both n_bits and input_range must have values or they must be None')
+
+        # for storing the signal
+        self._store_signal = store_signal
+        self.input_signal = None
+        self.input_bin_edges = None
+        self.output_signal = None
+        self.output_bin_edges = None
+
+    def process(self,bin_edges,signal,slice_sets,phase_advance, ** kwargs):
+        output_bin_edges, output_signal = self._resampler.process(bin_edges,signal,slice_sets,phase_advance, ** kwargs)
+
+        if self._digitizer is not None:
+            output_bin_edges, output_signal = self._digitizer.process(output_bin_edges, output_signal,slice_sets,phase_advance, ** kwargs)
+
+        if self._store_signal:
+            self.input_signal = np.copy(signal)
+            self.input_bin_edges = np.copy(bin_edges)
+            self.output_signal = np.copy(output_signal)
+            self.output_bin_edges = np.copy(output_bin_edges)
+
+        return output_bin_edges, output_signal
+
+
+class DAC(object):
+    def __init__(self,sampling_rate = None, n_bits = None, output_range = None, store_signal = False, **kwargs):
+        """ A model for an digital to analog converter, which changes a length of the input signal to correspond to
+            the number of slices in the PyHEADTAIL. If parameters for the quantizer are given, it quantizes also
+            the input signal to discrete levels.
+        :param sampling_rate: sampling rate of the ADC [Hz]
+        :param n_bits: the number of bits where to input signal is quantized. If the value is None, the input signal
+                is not quantizated. The default value is None.
+        :param input_range: the range for for the quantizer. If the value is None, the input signal is not quantizated.
+                The default value is None.
+        :param sync_method: The time range of the input signal might not correspond to an integer number of
+            samples determined by sampling rate.
+                'rounded': The time range of the input signal is divided to number of samples, which correspons to
+                    the closest integer of samples determined by the sampling rate (defaul)
+                'rising_edge': the exact value of the sampling rate is used, but there are empty space in the end
+                    of the signal
+                'falling_edge': the exact value of the sampling rate is used, but there are empty space in the beginning
+                    of the signal
+                'middle': the exact value of the sampling rate is used, but there are an equal amount of empty space
+                    in the beginning and end of the signal
+        """
+        self.label = 'DAC'
+
+        if sampling_rate is None:
+            self._resampler = Resampler('original',data_conversion = 'interpolation', **kwargs)
+
+        else:
+            self._resampler = Resampler('reconstructed', sampling_rate,data_conversion = 'interpolation')
+        self.required_variables = copy.copy(self._resampler.required_variables)
+
+        self._digitizer = None
+        if (n_bits is not None) and (output_range is not None):
+            self._digitizer = Quantizer(n_bits,output_range, **kwargs)
+            self.required_variables += self._digitizer.required_variables
+        elif (n_bits is not None) or (output_range is not None):
+            raise ValueError('Either both n_bits and input_range must have values or they must be None')
+
+        # for storing the signal
+        self._store_signal = store_signal
+        self.input_signal = None
+        self.input_bin_edges = None
+        self.output_signal = None
+        self.output_bin_edges = None
+
+    def process(self,bin_edges,signal,slice_sets,phase_advance, ** kwargs):
+        output_bin_edges, output_signal = self._resampler.process(bin_edges,signal,slice_sets,phase_advance, ** kwargs)
+
+        if self._digitizer is not None:
+            output_bin_edges, output_signal = self._digitizer.process(output_bin_edges, output_signal,slice_sets,phase_advance, ** kwargs)
+
+        if self._store_signal:
+            self.input_signal = np.copy(signal)
+            self.input_bin_edges = np.copy(bin_edges)
+            self.output_signal = np.copy(output_signal)
+            self.output_bin_edges = np.copy(output_bin_edges)
+
+        return output_bin_edges, output_signal
+
+
+class BackToOriginalSliceSets(Resampler):
+    def __init__(self, **kwargs):
+        if 'data_conversion' in kwargs:
+            super(self.__class__, self).__init__('original', **kwargs)
+        else:
+            super(self.__class__, self).__init__('original',data_conversion = 'interpolation', **kwargs)
+
+
+class UpSampler(Resampler):
+    def __init__(self, multiplier, kernel = None, **kwargs):
+
+        if kernel is None:
+            kernel = [0.]*multiplier
+            kernel[0] = 1.
+
+        data_conversion = ('kernel',kernel)
+
+        sampling_rate = ('multiplied', multiplier)
+
+        if 'data_conversion' in kwargs:
+            super(self.__class__, self).__init__('reconstructed',sampling_rate, sync_method='rising_edge', **kwargs)
+        else:
+            super(self.__class__, self).__init__('reconstructed',sampling_rate, sync_method='rising_edge',
+                                                 data_conversion = data_conversion, **kwargs)
+        self.label = 'UpSampler'
+
+
+class DownSampler(Resampler):
+    def __init__(self,multiplier,**kwargs):
+
+        sampling_rate = ('multiplied', multiplier)
+
+        if 'data_conversion' in kwargs:
+            super(self.__class__, self).__init__('reconstructed',sampling_rate, **kwargs)
+        else:
+            super(self.__class__, self).__init__('reconstructed',sampling_rate,data_conversion = 'bin_average',
+                                                 sync_method='rising_edge', **kwargs)
+        self.label = 'DownSampler'
