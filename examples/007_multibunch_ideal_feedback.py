377495cbfe5c73e11602606f5490801e1796d3c0
Jani Komppula <jani.komppula@cern.ch>
2017-01-20 16:13:56 +0100

    Multibunch tests

--- examples/007_multibunch_ideal_feedback.py
+++ examples/007_multibunch_ideal_feedback.py
@@ -1,6 +1,11 @@
-# run this file by using command:
-#$ mpirun -np 4 python FillingScheme_and_Feedback_Test.py
+# This file can be run by using the following command:
+#$ mpirun -np 4 python 007_multibunch_ideal_feedback.py
 
+"""
+    This a simple example for a multi bunch MPI feedback. It is based on the ideal bunch feedback presented
+    in the file 001_ideal_feedbacks.ipynb. The only difference is that multiple bunches are simulated in parallel
+    in this example.
+"""
 
 from __future__ import division
 
@@ -20,8 +25,23 @@ from PyHEADTAIL_feedback.feedback import OneboxFeedback
 from PyHEADTAIL_feedback.processors.multiplication import ChargeWeighter
 from PyHEADTAIL_feedback.processors.misc import Bypass
 
+plt.switch_backend('TkAgg')
+sns.set_context('talk', font_scale=1.3)
+sns.set_style('darkgrid', {
+    'axes.edgecolor': 'black',
+    'axes.linewidth': 2,
+    'lines.markeredgewidth': 1})
+
+
 
 def pick_signals(processor, source = 'input'):
+    """
+    A function which helps to visualize the signals passing the signal processors.
+    :param processor: a reference to the signal processor
+    :param source: source of the signal, i.e, 'input' or 'output' signal of the processor
+    :return: (t, z, bins, signal), where 't' and 'z' are time or position values for the signal values (which can be used
+        as x values for plotting), 'bins' are data for visualizing sampling and 'signal' is the actual signal.
+    """
 
     if source == 'input':
         bin_edges = processor.input_signal_parameters.bin_edges
@@ -32,15 +52,10 @@ def pick_signals(processor, source = 'input'):
     else:
         raise ValueError('Unknown value for the data source')
 
-    print 'len(bin_edges): ' + str(len(bin_edges))
-    print 'len(raw_signal): ' + str(len(raw_signal))
-
-    t = np.zeros(len(raw_signal)*4)
     z = np.zeros(len(raw_signal)*4)
     bins = np.zeros(len(raw_signal)*4)
     signal = np.zeros(len(raw_signal)*4)
     value = 1.
-
 
     for i, edges in enumerate(bin_edges):
         z[4*i] = edges[0]
@@ -63,50 +78,66 @@ def pick_signals(processor, source = 'input'):
 
 
 def kicker(bunch):
+    """
+    A function which sets initial kicks for the bunches. The function is passed to the bunch generator
+    in the machine object.
+    """
     bunch.x *= 0
     bunch.xp *= 0
     bunch.y *= 0
     bunch.yp *= 0
     bunch.x[:] += 2e-2 * np.sin(2.*pi*np.mean(bunch.z)/1000.)
 
-plt.switch_backend('TkAgg')
-sns.set_context('talk', font_scale=1.3)
-sns.set_style('darkgrid', {
-    'axes.edgecolor': 'black',
-    'axes.linewidth': 2,
-    'lines.markeredgewidth': 1})
 
-
+# MPI objects, which are used for visualizing the data only on the first processor
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+# SIMULATION, BEAM AND MACHNINE PARAMETERS
+# ========================================
 n_turns = 100
-chroma = 0
-
 n_segments = 1
-n_bunches = 13
-filling_scheme = [401 + 20*i for i in range(n_bunches)]
 n_macroparticles = 40000
-intensity = 2.3e11
 
-
-# BEAM AND MACHNINE PARAMETERS
-# ============================
 from test_tools import MultibunchMachine
 machine = MultibunchMachine(n_segments=n_segments)
 
+intensity = 2.3e11
 epsn_x = 2.e-6
 epsn_y = 2.e-6
 sigma_z = 0.081
 
+
+# FILLING SCHEME
+# ==============
+# Bunches are created by creating a list of numbers which represent bunches in the RF buckets.
+# In the other words, this means that the length of the list corresponds to the number of bunches are simulated and
+# the numbers in the list correspond to the locations of the bunches in the machine.
+
+n_bunches = 13
+filling_scheme = [401 + 20*i for i in range(n_bunches)]
+
+# multiple bunches are created by passing the filling scheme to the generator. It returns a super bunch, which contains
+# particles from the all of the bunches, but can be split into separated bunches
 bunches = machine.generate_6D_Gaussian_bunch_matched(
     n_macroparticles, intensity, epsn_x, epsn_y, sigma_z=sigma_z,
     filling_scheme=filling_scheme, kicker=kicker)
 
+
 # CREATE BEAM SLICERS
 # ===================
 slicer = UniformBinSlicer(50, n_sigma_z=3)
+
+
+# FEEDBACK MAP
+# ==============
+# Actual PyHEADTAIL map for the feedback system is created here. It is exactly same as presented for a single bunch
+# in the file '001_ideal_feedbacks.ipynb'. Only difference is that 'mpi' flag is set into 'True' in OneboxFeedback
+# object.
+#
+# Flags 'store_signal' are set into 'True' in the signal processors in order to visualize signal processing after the
+# simulation, However, the flag does not affect the actual simulation.
 
 processors_x = [
     Bypass(store_signal = True),
@@ -116,23 +147,20 @@ processors_y = [
     Bypass(store_signal = True),
     ChargeWeighter(normalization = 'average',store_signal  = True),
 ]
-
 gain = 0.1
-
 feedback_map = OneboxFeedback(gain, slicer, processors_x, processors_y, axis='displacement', mpi = True)
 
+# The map is included directly into the total map in the machine.
+machine.one_turn_map.append(feedback_map)
 
 # TRACKING LOOP
 # =============
-machine.one_turn_map.append(feedback_map)
-# machine.one_turn_map.append(wake_field)
-
 s_cnt = 0
 monitorswitch = False
+
 if rank == 0:
     print '\n--> Begin tracking...\n'
 
-print 'Tracking'
 for i in range(n_turns):
 
     if rank == 0:
@@ -144,28 +172,29 @@ for i in range(n_turns):
         print('Turn {:d}, {:g} ms, {:s}'.format(i, (t1-t0)*1e3, time.strftime(
             "%d/%m/%Y %H:%M:%S", time.localtime())))
 
+# VISUALIZATION
+# =============
 if rank == 0:
+    # On the first processor, the script plots signals passed each signal processor from the last simulated turn
+
     fig, (ax1, ax2) = plt.subplots(2, figsize=(14, 14), sharex=False)
 
     for i, processor in enumerate(processors_x):
-        print 'Processor: ' + str(i)
         t, z, bins, signal = pick_signals(processor,'output')
-        ax1.plot(z, bins, label =  processor.label)
+        ax1.plot(z, bins*(0.9**i), label =  processor.label)
         ax2.plot(z, signal, label =  processor.label)
-        if i == 0:
-            print t
-            print z
-            print bins
-            print signal
 
-    # ax3.plot(processors_x[0]._CDF_time,processors_x[0]._PDF,'r-')
+    # The first plot represents sampling in the each signal processor. The magnitudes of the curves do not represent
+    # anything, but the change of the polarity represents a transition from one bin to other.
+    ax1.set_ylim([-1.1, 1.1])
     ax1.set_xlabel('Z position [m]')
-    ax1.set_ylabel('Bins')
+    ax1.set_ylabel('Bin set')
+    ax1.legend(loc='upper left')
 
-
+    # Actual signals are plotted in this figure
     ax2.set_xlabel('Z position [m]')
-    ax2.set_ylabel('Signal after the processor')
-
+    ax2.set_ylabel('Signal')
+    ax2.legend(loc='upper left')
 
     plt.legend()
     plt.show()
--- examples/008_multibunch_separated_pickup_and_kicker.py
+++ examples/008_multibunch_separated_pickup_and_kicker.py
@@ -1,5 +1,11 @@
-# run this file by using command:
-#$ mpirun -np 4 python FillingScheme_and_Feedback_Test.py
+# This file can be run by using the following command:
+#$ mpirun -np 4 python 008_multibunch_separated_pickup_and_kicker.py
+
+"""
+    This example represent implementation of a feedback system consisting of a separate pickup and kicker.  It is based
+    on the example presented in the file '002_separated_pickup_and_kicker.ipynb'. The only difference is that multiple
+    bunches are simulated in parallel in this example.
+"""
 
 
 from __future__ import division
@@ -23,8 +29,23 @@ from PyHEADTAIL_feedback.processors.misc import Bypass
 from PyHEADTAIL_feedback.processors.register import HilbertPhaseShiftRegister
 from PyHEADTAIL_feedback.processors.signal import BeamParameters
 
+plt.switch_backend('TkAgg')
+sns.set_context('talk', font_scale=1.3)
+sns.set_style('darkgrid', {
+    'axes.edgecolor': 'black',
+    'axes.linewidth': 2,
+    'lines.markeredgewidth': 1})
+
+
 
 def pick_signals(processor, source = 'input'):
+    """
+    A function which helps to visualize the signals passing the signal processors.
+    :param processor: a reference to the signal processor
+    :param source: source of the signal, i.e, 'input' or 'output' signal of the processor
+    :return: (t, z, bins, signal), where 't' and 'z' are time or position values for the signal values (which can be used
+        as x values for plotting), 'bins' are data for visualizing sampling and 'signal' is the actual signal.
+    """
 
     if source == 'input':
         bin_edges = processor.input_signal_parameters.bin_edges
@@ -35,15 +56,10 @@ def pick_signals(processor, source = 'input'):
     else:
         raise ValueError('Unknown value for the data source')
 
-    print 'len(bin_edges): ' + str(len(bin_edges))
-    print 'len(raw_signal): ' + str(len(raw_signal))
-
-    t = np.zeros(len(raw_signal)*4)
     z = np.zeros(len(raw_signal)*4)
     bins = np.zeros(len(raw_signal)*4)
     signal = np.zeros(len(raw_signal)*4)
     value = 1.
-
 
     for i, edges in enumerate(bin_edges):
         z[4*i] = edges[0]
@@ -66,50 +82,66 @@ def pick_signals(processor, source = 'input'):
 
 
 def kicker(bunch):
+    """
+    A function which sets initial kicks for the bunches. The function is passed to the bunch generator
+    in the machine object.
+    """
     bunch.x *= 0
     bunch.xp *= 0
     bunch.y *= 0
     bunch.yp *= 0
     bunch.x[:] += 2e-2 * np.sin(2.*pi*np.mean(bunch.z)/1000.)
 
-plt.switch_backend('TkAgg')
-sns.set_context('talk', font_scale=1.3)
-sns.set_style('darkgrid', {
-    'axes.edgecolor': 'black',
-    'axes.linewidth': 2,
-    'lines.markeredgewidth': 1})
 
-
+# MPI objects, which are used for visualizing the data only on the first processor
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+# SIMULATION, BEAM AND MACHNINE PARAMETERS
+# ========================================
 n_turns = 100
-chroma = 0
-
 n_segments = 1
-n_bunches = 13
-filling_scheme = [401 + 20*i for i in range(n_bunches)]
 n_macroparticles = 40000
-intensity = 2.3e11
 
-
-# BEAM AND MACHNINE PARAMETERS
-# ============================
 from test_tools import MultibunchMachine
 machine = MultibunchMachine(n_segments=n_segments)
 
+intensity = 2.3e11
 epsn_x = 2.e-6
 epsn_y = 2.e-6
 sigma_z = 0.081
 
+
+# FILLING SCHEME
+# ==============
+# Bunches are created by creating a list of numbers which represent bunches in the RF buckets.
+# In the other words, this means that the length of the list corresponds to the number of bunches are simulated and
+# the numbers in the list correspond to the locations of the bunches in the machine.
+
+n_bunches = 13
+filling_scheme = [401 + 20*i for i in range(n_bunches)]
+
+# multiple bunches are created by passing the filling scheme to the generator. It returns a super bunch, which contains
+# particles from the all of the bunches, but can be split into separated bunches
 bunches = machine.generate_6D_Gaussian_bunch_matched(
     n_macroparticles, intensity, epsn_x, epsn_y, sigma_z=sigma_z,
     filling_scheme=filling_scheme, kicker=kicker)
 
+
 # CREATE BEAM SLICERS
 # ===================
 slicer = UniformBinSlicer(50, n_sigma_z=3)
+
+
+# FEEDBACK MAP
+# ==============
+# Actual PyHEADTAIL map for the feedback system is created here. It is exactly same as presented for a single bunch
+# in the file '002_separated_pickup_and_kicker.ipynb'. Only difference is that 'mpi' flag is set into 'True' in OneboxFeedback
+# object.
+#
+# Flags 'store_signal' are set into 'True' in the signal processors in order to visualize signal processing after the
+# simulation, However, the flag does not affect the actual simulation.
 
 delay = 1
 n_values = 3
@@ -146,8 +178,10 @@ gain = 0.1
 kicker_map = Kicker(gain, slicer, kicker_processors_x, kicker_processors_y,
                     registers_x, registers_y, kicker_beam_parameters_x, kicker_beam_parameters_y, mpi = True)
 
-# TRACKING LOOP
-# =============
+
+# The one turn map of the machine is reconstructed and the kicker and the pickup are placed into the correct slots.
+# The one turn map is created machine in suck a way that the first elements of the map are transverse elements
+# (but this might vary!).
 
 new_one_turn_map = []
 for i, m in enumerate(machine.one_turn_map):
@@ -162,6 +196,8 @@ for i, m in enumerate(machine.one_turn_map):
 
 machine.one_turn_map = new_one_turn_map
 
+# TRACKING LOOP
+# =============
 s_cnt = 0
 monitorswitch = False
 if rank == 0:
@@ -180,53 +216,44 @@ for i in range(n_turns):
             "%d/%m/%Y %H:%M:%S", time.localtime())))
 
 if rank == 0:
+    # On the first processor, the script plots signals passed each signal processor from the last simulated turn
+
     fig, (ax1, ax2) = plt.subplots(2, figsize=(14, 14), sharex=False)
     fig.suptitle('Pickup processors', fontsize=20)
-    for i, processor in enumerate(pickup_processors_x):
-        print 'Processor: ' + str(i)
-        t, z, bins, signal = pick_signals(processor,'output')
-        ax1.plot(z, bins*(1.-0.1*i), label =  processor.label)
-        ax2.plot(z, signal, label =  processor.label)
-        if i == 0:
-            print t
-            print z
-            print bins
-            print signal
 
-    # ax3.plot(processors_x[0]._CDF_time,processors_x[0]._PDF,'r-')
+    for i, processor in enumerate(pickup_processors_x):
+        t, z, bins, signal = pick_signals(processor,'output')
+        ax1.plot(z, bins*(0.9**i), label =  processor.label)
+        ax2.plot(z, signal, label =  processor.label)
+
+    # The first plot represents sampling in the each signal processor. The magnitudes of the curves do not represent
+    # anything, but the change of the polarity represents a transition from one bin to other.
     ax1.set_ylim([-1.1, 1.1])
     ax1.set_xlabel('Z position [m]')
     ax1.set_ylabel('Bin set')
     ax1.legend(loc='upper left')
 
-
-
+    # Actual signals are plotted in this figure
     ax2.set_xlabel('Z position [m]')
     ax2.set_ylabel('Signal')
     ax2.legend(loc='upper left')
 
-
     fig, (ax3, ax4) = plt.subplots(2, figsize=(14, 14), sharex=False)
     fig.suptitle('Kicker processors', fontsize=20)
 
-
     for i, processor in enumerate(kicker_processors_x):
-        print 'Processor: ' + str(i)
         t, z, bins, signal = pick_signals(processor,'output')
-        ax3.plot(z, bins*(1.-0.1*i), label =  processor.label)
+        ax3.plot(z, bins*(0.9**i), label =  processor.label)
         ax4.plot(z, signal, label =  processor.label)
-        if i == 0:
-            print t
-            print z
-            print bins
-            print signal
 
-    # ax3.plot(processors_x[0]._CDF_time,processors_x[0]._PDF,'r-')
+    # The first plot represents sampling in the each signal processor. The magnitudes of the curves do not represent
+    # anything, but the change of the polarity represents a transition from one bin to other.
     ax3.set_ylim([-1.1, 1.1])
     ax3.set_xlabel('Z position [m]')
     ax3.set_ylabel('Bin set')
     ax3.legend(loc='upper left')
 
+    # Actual signals are plotted in this figure
     ax4.set_xlabel('Z position [m]')
     ax4.set_ylabel('Signal')
     ax4.legend(loc='upper left')
